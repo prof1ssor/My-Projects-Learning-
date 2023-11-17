dd78573621eff2ac5f27ea375b4bf0c8d8e194d4
Professor <120298988+prof1ssor@users.noreply.github.com>
2023-11-17 10:00:27 -0500

    Create Basic_string.cpp

---
+++ Basic_string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout << "Enter the your word : ";
+    cin >> str;
+
+    cout << str << endl;
+
+    return 0;
+}

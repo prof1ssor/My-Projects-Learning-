6afeaf13a976d483a843da9b4996650eadaed376
Professor <120298988+prof1ssor@users.noreply.github.com>
2024-01-11 21:05:04 -0500

    Update class_cin.cpp

--- class_cin.cpp
+++ class_cin.cpp
@@ -15,13 +15,11 @@ int main(){
     happy r1,r2;
     std::cout << "Enter the Length and Breath of the rectangle for the area : ";
     std::cin >> r1.length;
-    std::cout << "\n";
     std::cin >> r1.breath;
     std::cout << "\nThe Area for the given rectangle is : " << r1.area() << std::endl;
     
     std::cout << "Enter the Length and Breath of the rectangle for the perimeter : ";
     std::cin >> r2.length;
-    std::cout << "\n";
     std::cin >> r2.breath;
     std::cout << "\nThe Area for the given rectangle is : " << r2.perimeter() << std::endl;
 
